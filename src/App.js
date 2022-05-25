502dfe8ea7ed7d8ffc7f507b21a46cb3ed1a7e05
AlexHome <foster13@yandex.ru>
2022-05-25 22:52:40 +0300

    add video

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
+import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
@@ -9,7 +13,18 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' exact/>
+          <Route path='/' exact
+            element={<Home/>}
+          />
+          <Route path='/Services' exact
+            element={<Services/>}
+          />
+          <Route path='/Products' exact
+            element={<Products/>}
+          />
+          <Route path='/Sign-up' exact
+            element={<SignUp/>}
+          />
         </Routes>
       </Router>
      
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import {Button} from '../components/Button';
 import "./Navbar.css";
 
 function Navbar() {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
   const handleClick = () => {
     setClick(!click);
   };
@@ -11,11 +14,25 @@ function Navbar() {
     setClick(false);
   }
 
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  }
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener('resize', showButton);
+
   return (
     <>
       <div className="navbar">
         <div className="navbar-container">
-          <Link to="/" className="navbar-logo">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
             TRVL 
             <img src="images/logo.svg" alt="logo" className="logo-image svg"/>
           </Link>
@@ -27,9 +44,16 @@ function Navbar() {
               <Link to='/' className="nav-links" onClick={closeMobileMenu}>Home</Link>
             </li>
             <li className="nav-item">
-              <Link to='/' className="nav-links" onClick={closeMobileMenu}>Services</Link>
+              <Link to='/services' className="nav-links" onClick={closeMobileMenu}>Services</Link>
+            </li>
+            <li className="nav-item">
+              <Link to='/products' className="nav-links" onClick={closeMobileMenu}>Products</Link>
+            </li>
+            <li className="nav-item">
+              <Link to='/sign-up' className="nav-links-mobile" onClick={closeMobileMenu}>Sign Up</Link>
             </li>
           </ul>
+          {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
         </div>
       </div>
     </>
